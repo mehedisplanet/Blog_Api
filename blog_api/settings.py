a0e96198d9d535ded6f254c2d16053b6f60199d6
unknown <you@example.com>
2024-03-18 21:10:08 +0600

    update

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -17,6 +17,7 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = env("SECRET_KEY")
 
 
 # Quick-start development settings - unsuitable for production
