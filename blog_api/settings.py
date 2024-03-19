73576263c5028288d4d19c35cbfc21cc26080f84
unknown <you@example.com>
2024-03-19 15:06:52 +0600

    update7

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://my-blog-api-p9hh.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://blog-w6km.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
